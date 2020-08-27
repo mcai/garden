18d116c69289b9e8de9794e0ec2b7aa7a0674b1c
Min Cai <min.cai.china@gmail.com>
2020-08-27 17:30:38 +0800

    Rename /entity to /entities.

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import {User} from "./entities/User";
 import {MongoConnectionOptions} from "typeorm/driver/mongodb/MongoConnectionOptions";
 
 export async function testConnection(option: MongoConnectionOptions) {
