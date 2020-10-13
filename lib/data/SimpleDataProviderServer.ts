1e6407f0ec159031cccdb464843f41ec6dbb3cee
Min Cai <min.cai.china@gmail.com>
2020-10-13 08:46:50 +0800

    Add SimpleMongoDbDataProviderServer.

---
+++ lib/data/SimpleDataProviderServer.ts
@@ -0,0 +1,48 @@
+export interface SimpleDataProviderServer {
+    all<ItemT>(
+        ordering?: any,
+        filter?: {
+            [key: string]: any
+        }
+    ): Promise<ItemT[] | undefined>;
+
+    find<ItemT>(
+        pageSize: number,
+        pageNum: number,
+        ordering?: any,
+        filter?: {
+            [key: string]: any
+        }
+    ): Promise<{
+        count: number,
+        pageCount: number,
+        itemsInCurrentPage: ItemT[]
+    } | undefined>;
+
+    one<RecordT>(
+        filter?: {
+            [key: string]: any
+        }
+    ): Promise<RecordT | undefined>
+
+    create<RecordT>(
+        data: {
+            [key: string]: any
+        }
+    ): Promise<RecordT | undefined>;
+
+    update<RecordT>(
+        id: number,
+        data: {
+            [key: string]: any
+        }
+    ): Promise<RecordT | undefined>;
+
+    remove<RecordT>(
+        id: number,
+        data: {
+            [key: string]: any
+        }
+    ): Promise<void>;
+}
+
