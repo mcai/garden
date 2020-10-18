543d0f06091b74569ab96b85851838b27946d0cf
Min Cai <min.cai.china@gmail.com>
2020-10-19 01:45:26 +0800

    Update SimpleServer.ts.

--- src/data/SimpleServer.ts
+++ src/data/SimpleServer.ts
@@ -15,7 +15,7 @@ export class SimpleServer {
         const app = express();
 
         app.use(express.json({ limit: "50mb" }));
-        app.use(express.urlencoded({ limit: "50mb" }));
+        app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
         app.use(cors());
 
