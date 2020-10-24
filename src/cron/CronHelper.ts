e8b1cfdda2046f663332d4d457bb40241c3289c1
Min Cai <min.cai.china@gmail.com>
2020-10-24 17:53:59 +0800

    Add CronHelper.ts.

---
+++ src/cron/CronHelper.ts
@@ -0,0 +1,5 @@
+export class CronHelper {
+    static everyMinute = "* * * * *";
+    static everyHour = "0 * * * *";
+    static everyDay = "0 0 * * *";
+}
--- src/demo.ts
+++ src/demo.ts
@@ -1,6 +1,7 @@
 import { listen } from "./index";
 import { Metrics } from "./math/Metrics";
 import { SocketIOClientHelper } from "./realtime/SocketIOClientHelper";
+import { CronHelper } from "./cron/CronHelper";
 
 const connectionString = "mongodb://localhost:27017";
 const port = 3721;
@@ -9,12 +10,12 @@ const socket = SocketIOClientHelper.createClient("localhost", port);
 
 listen(connectionString, port, undefined, [
     {
-        every: "* * * * *",
+        every: CronHelper.everyMinute,
         name: "note.count",
         action: (dataProvider) => Metrics.count(dataProvider, "note", {}),
     },
     {
-        every: "* * * * *",
+        every: CronHelper.everyMinute,
         name: "socket.io.echo",
         action: (dataProvider) => {
             socket.emit("echo", "hello");
