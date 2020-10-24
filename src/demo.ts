8543ce5265938c33057401cd95846c5c702293f6
Min Cai <min.cai.china@gmail.com>
2020-10-24 14:50:55 +0800

    Update SimpleCronPlanScheduler.ts.

--- src/demo.ts
+++ src/demo.ts
@@ -6,12 +6,12 @@ import { SimpleDataProvider } from "./dataProviders/SimpleDataProvider";
 const connectionString = "mongodb://localhost:27017";
 const port = 3721;
 
-async function count(dataProvider: SimpleDataProvider, resource: string, filter: any, action: any) {
+async function count(dataProvider: SimpleDataProvider, resource: string, filter: any) {
     const count = await dataProvider.countOne(resource, filter);
     console.log(
-        `[${SimpleFormatting.toFormattedDateTimeString(moment())} SimpleServer::${action.name}] every=${
-            action.every
-        }, resource=${resource}, filter=${JSON.stringify(filter)}, count=${count.data}`,
+        `[${SimpleFormatting.toFormattedDateTimeString(
+            moment(),
+        )} SimpleServer::count] resource=${resource}, filter=${JSON.stringify(filter)}, count=${count.data}`,
     );
 }
 
@@ -19,6 +19,6 @@ listen(connectionString, port, undefined, [
     {
         every: "* * * * *",
         name: "note.count",
-        action: (dataProvider) => count(dataProvider, "note", {}, {}),
+        action: (dataProvider) => count(dataProvider, "note", {}),
     },
 ]);
