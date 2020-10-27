1bed033b978de2f1fa83a3251272ed0d1a4b118d
Min Cai <min.cai.china@gmail.com>
2020-10-27 13:20:26 +0800

    Update CronHelper.ts.

--- src/cron/CronHelper.ts
+++ src/cron/CronHelper.ts
@@ -1,5 +1,6 @@
 export class CronHelper {
     static everyMinute = "* * * * *";
     static everyHour = "0 * * * *";
-    static everyDay = "0 0 * * *";
+    static everyday_0am = "0 0 * * *";
+    static everyday_10am = "0 10 * * *";
 }
